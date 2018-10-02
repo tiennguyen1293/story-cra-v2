b2a740c26c8d0f9eb565fae0b63d154bd3410873
tien.nguyen <tien.nguyen@quoine.com>
2018-10-02 14:07:12 +0700

    restructure workspace

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
-import { simpleAction } from './actions'
+import { simpleAction } from './App/actions'
 
 class App extends React.Component {
   simpleAction = (event) => {
